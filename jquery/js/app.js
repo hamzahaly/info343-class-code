d8eb63a5ca0e5970e2270689ea233e4aa6e80d46
hamzahaly <hzhaly93@gmail.com>
2015-10-15 17:12:16 -0700

    Notes that will help with Challenge 3

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -5,4 +5,28 @@
     open weather API: 
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
+/* where we write all of our jQuery */
 
+// when the DOM content has been loaded; synonymous to what we've been doing in JavaScript, but way easier.
+$(function() {
+    'use strict'
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn(1000);
+    $('#toggle-article').click(function() {
+       $('article').fadeToggle();
+    });
+
+    // For the Next Challenge, to get traffic cam data.
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+    $.getJSON(url).then(function(data) {
+        console.log(data);
+        // Select the data from the console.
+        var temperature = data.main.temp;
+        // Select Span element with the #temp id.
+        $('#temp').text(Math.round(temperature));
+    });
+
+    console.log('test');
+
+});
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,34 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+
+$(function() {
+    'use strict'
+
+    function createMap(loc, zoom) {
+        // Copy and Pasted from the Leaflet.js website.
+        var map = L.map('map').setView(loc, zoom);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            // Required by law. Give them credit where it's due.
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // Creating Markers, look at their API to find what kinds of methods you can use. ex. circleMarker, marker.
+        // Markers are SVGs.
+        L.circleMarker(loc).addTo(map).bindPopup('<h2>Hello</h2>');
+
+        // bindPopup accepts any html. ex. h2, img, p, etc.
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+           createMap([pos.coords.latitude, pos.coords.longitude], 15);
+        });
+    }
+    else {
+        createMap([47.6097, -122.3331], 12);
+    }
+    // Iterate over the JSON with .forEach
+    // Each element is an object that have properties that will be important.
+});
