712a5bdd1b018f510878c6bf26d44ae4a75ca9c2
hamzahaly <hzhaly93@gmail.com>
2015-11-10 16:39:42 -0800

    notes on how to work as a team

--- firebase-auth/js/app.js
+++ firebase-auth/js/app.js
@@ -78,3 +78,57 @@ angular.module('ChatApp', ['firebase'])
         }; //sendMessage()
 
     }); //ChatController
+
+// new data and auth are magic variables in Firebase
+
+// Gonna take notes about team Github stuff
+
+/* New repository: Team Demo 2
+ Descr
+ Initialize with Read Me
+ Can add git.ignore or do it manually
+ adding people:
+ Settings > Collaborators >
+ The newly added collaborator should clone (actually both should clone)
+ File a new bug under Issues Module when your teammates introduce new bugs
+ Assign issues to collaborators
+ Don't party on the master branch
+ Create a new branch in the repo for each new feature
+ Create and do things in the branch
+ git checkout -b "some name"(switches branches and creates a new branch)Different names please
+ git push origin "name of feature branch" (origin is the repo from which it came or was cloned from)
+ create a pull request:
+    to ask team members to review it
+    merge pull request to confirm the merge into the master branch
+    #issue number can automatically close the issue for you
+    go back to master: git checkout master
+    git pull origin master (pulls Caleb's changes)
+
+    git merge master: merge all the things from master into the feature branch
+    gitk: opens a graphical display
+    git branch to see what branch you are on
+    git push origin feature2 (Pushes the feature2 branch)
+
+    Something is wrong with the code, don't merge right away bounce back to the sender
+    All code is isolated from Master until we want to merge the code
+
+    git branch
+    git status
+    git commit -am "resolves #2 for real" (adds and commits in one line)
+    git push origin feature2
+
+    other dude must merge and then confirm the merge then close the pull request
+    git branch -d feature2 (delete the branch)
+
+    I MADE A HORRIBLE MISTAKE (What if you did the commit)
+    git revert HEAD (revert the last commit made in the repo, HEAD refers to the last commit made in repo)
+    undoes the commit
+    -1, -2, -3, HEAD does a new opposite commit.
+
+    Release Version 1
+    git tag -a v1.0 -m "release 1.0"
+    git push origin master doesn't push the tags
+    git push --tags
+    This pushes the tags
+
+ */
